cc48b8e3b9955af3461091d18580b9f8efc6945d
alekseygenerozov <aleksey.generozov@gmail.com>
2019-03-29 15:48:29 -0600

    Minor changes to grid generating code.

--- gen_grid.py
+++ gen_grid.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-a1s=np.arange(0., 1.01, 0.1)
-a1s[0]=0.01
-# a1s=[1.0]
+# a1s=np.arange(0., 1.01, 0.1)
+# a1s[0]=0.01
+a1s=[0.99]
 angs=np.arange(0.0, 180.1, 5.)
 e1s=np.arange(0, 0.91, 0.1)
 e1s[0]=0.01
@@ -19,9 +19,9 @@ for e1 in e1s:
 			temp=temp.replace('xx1', '{0}'.format(e1))
 			temp=temp.replace('xx', '--etest {0}'.format(e1))
 			temp=temp.replace('yy1', '{0}'.format(a1))
-			temp=temp.replace('yy', '--atest {0:.1g}'.format(a1))
+			temp=temp.replace('yy', '--atest {0:.2f}'.format(a1))
 			temp=temp.replace('zz1', '{0}'.format(ang))
 			temp=temp.replace('zz', '--pomega {0:.1f} -q -1.6 --ein 0.9'.format(ang))
-			f=open('e{0}_a{1:.1g}_ang{2:.1f}.sh'.format(e1, a1, ang), 'w')
+			f=open('e{0}_a{1:.2f}_ang{2:.1f}.sh'.format(e1, a1, ang), 'w')
 			f.write(temp)
 			f.close()
--- post_process.py
+++ post_process.py
@@ -5,8 +5,9 @@ import shlex
 
 import json
 
-tags=glob.glob("*ein*")
+tags=glob.glob("[it]*ein*")
 tags=np.sort(tags)
+print tags
 prec_rate={}
 for tag in tags:
 	x=bc.bash_command("cat {0}".format(tag))
